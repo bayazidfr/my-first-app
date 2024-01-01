54b3e94310dd448036cec8de1fd30fb5263567ab
bayazidfr <144949680+bayazidfr@users.noreply.github.com>
2024-01-01 19:17:17 +0400

    Initialize state for ContactPage with formData and messageSent

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,3 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './ContactPage.css'; 
+
+const ContactPage = () => {
+  const [formData, setFormData] = useState({
+    subject: '',
+    email: '',
+    content: ''
+  });
+  const [messageSent, setMessageSent] = useState(false);
